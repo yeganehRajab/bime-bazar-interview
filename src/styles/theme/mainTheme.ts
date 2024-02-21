da50c41fd74f06192e788e29c6baa1d45ab3ec5f
Yeganeh Rajabi <yrajabi940@gmail.com>
2024-02-21 11:26:49 +0330

    feat: handle theme and mui provider

---
+++ src/styles/theme/mainTheme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const mainTheme = createTheme({
+  direction: "rtl",
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
+
+export default mainTheme;
