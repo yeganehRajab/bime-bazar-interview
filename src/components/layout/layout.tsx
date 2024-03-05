8571a9de2202d953a16ca2a54ab93639fd036217
y.rajabi <yrajabi940@gmail.com>
2024-03-05 11:24:17 +0330

    [fix]: fix error on build

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,7 +2,6 @@ import { Stack } from "@mui/material";
 import React, { FC } from "react";
 import { layoutStyles } from "./layout.styles";
 import { IMainLayout } from "./layout.types";
-import MainMenu from "../ui/menu/menu";
 
 const MainLayout: FC<IMainLayout> = ({ children }) => {
   return (
